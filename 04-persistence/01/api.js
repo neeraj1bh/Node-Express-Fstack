a7ff23d3b28b24077169d7b227b89bb74467279a
Neeraj Bhatt <neeraj1bh@gmail.com>
2021-07-12 22:55:35 +0530

    added validation and created model with mongo to save in mongoose instead of saving locally with fs module.

---
+++ 04-persistence/01/api.js
@@ -0,0 +1,44 @@
+const Products = require("./products");
+const autoCatch = require("./lib/auto-catch");
+
+async function listProducts(req, res) {
+  const { offset = 0, limit = 25, tag } = req.query;
+  res.json(
+    await Products.list({
+      offset: Number(offset),
+      limit: Number(limit),
+      tag,
+    })
+  );
+}
+
+async function getProduct(req, res, next) {
+  const { id } = req.params;
+  const product = await Products.get(id);
+  if (!product) return next();
+  return res.json(product);
+}
+
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body);
+  res.json(product);
+}
+
+async function updateProduct(req, res, next) {
+  const change = req.body;
+  const product = await Products.edit(req.params.id, change);
+  res.json(product);
+}
+
+async function deleteProduct(req, res, next) {
+  const product = await Products.remove(req.params.id);
+  res.json({ success: true });
+}
+
+module.exports = autoCatch({
+  listProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+});
