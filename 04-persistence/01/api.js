b2c2aabe2a619dfcec418e2841d79573f7f4fc2c
Neeraj Bhatt <neeraj1bh@gmail.com>
2021-07-13 13:23:40 +0530

    added orders routes and data import in local mongo db database

--- 04-persistence/01/api.js
+++ 04-persistence/01/api.js
@@ -1,4 +1,5 @@
-const Products = require("./products");
+const Products = require("./models/products");
+const Orders = require("./models/orders");
 const autoCatch = require("./lib/auto-catch");
 
 async function listProducts(req, res) {
@@ -35,10 +36,35 @@ async function deleteProduct(req, res, next) {
   res.json({ success: true });
 }
 
+async function createOrder(req, res, next) {
+  const order = await Orders.create(req.body);
+  res.json(order);
+}
+
+async function get(_id) {
+  const order = await Order.findById(_id).populate("products").exec();
+  return order;
+}
+
+async function listOrders(req, res, next) {
+  const { offset = 0, limit = 25, status, productId } = req.query;
+
+  const orders = await Orders.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    productId,
+    status,
+  });
+
+  res.json(orders);
+}
+
 module.exports = autoCatch({
   listProducts,
   getProduct,
   createProduct,
   updateProduct,
   deleteProduct,
+  createOrder,
+  listOrders,
 });
--- 04-persistence/01/server-01.js
+++ 04-persistence/01/server-01.js
@@ -8,11 +8,20 @@ const app = express();
 
 app.use(middleware.cors);
 app.use(express.json());
+
+// Products endpoints
 app.get("/products", api.listProducts);
 app.post("/products", api.createProduct);
 app.get("/products/:id", api.getProduct);
 app.put("/products/:id", api.updateProduct);
 app.delete("/products/:id", api.deleteProduct);
+
+// Orders endpoints
+app.get("/orders", api.listOrders);
+app.post("/orders", api.createOrder);
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// Error handler and Not Found Page handlers
 app.use(middleware.handleError);
 app.use(middleware.notFound);
